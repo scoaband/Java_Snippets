bec343cdb53934365611d0b8a1c0247f2617dd43
Johnathan Baez <scoaband@hotmail.com>
2019-08-27 14:35:02 -0400

    Fl Salary Calculator JAVA

--- Salary_Calculator/Wage_Calculator.java
+++ Salary_Calculator/Wage_Calculator.java
@@ -5,19 +5,35 @@ public class Wage_Calculator {
 	public static void main(String[] args) {
 		
 		
-		System.out.println(salary());
+		salary();
 		
 	}
 	
-	static int salary(){
-		int wage;
+	static void salary(){
+		double wage;
+		long Fl_Under10tax;
+		long Fl_great10tax;
+		double yearWage;
 		
-		System.out.println("Hourly Salary: ");
+		System.out.print("Hourly Salary: ");
 		Scanner sc = new Scanner(System.in);
 		wage = sc.nextInt();
 	    sc.close();
+	    
 	    wage = wage * 40 * 4 * 12;
-	      
-		return wage;
+	    System.out.println("Pre-Taxes Year Salary: " + wage);
+	    
+	    if(wage <= 10000) {
+	    	
+		    Fl_Under10tax = (long) (0.1 * wage);
+		    yearWage = wage - Fl_Under10tax;
+		    System.out.println("Year Salary: " + yearWage);
+	    }
+	    else {
+	    	Fl_great10tax = (long) (0.20 * wage);
+	    	yearWage = wage - Fl_great10tax;
+		    System.out.println("Year Salary: " + yearWage);
+	    }
+	    
 	}
 }
